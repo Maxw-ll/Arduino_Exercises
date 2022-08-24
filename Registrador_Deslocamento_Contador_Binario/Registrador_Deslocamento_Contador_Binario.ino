ec08396cc30645fbbb9d7bb7d95d11bff66783e8
Maxwell <maxwellpires2002@gmail.com>
2022-08-23 22:38:39 -0300

    Loop e funções adicionadas

--- Registrador_Deslocamento_Contador_Binario/Registrador_Deslocamento_Contador_Binario.ino
+++ Registrador_Deslocamento_Contador_Binario/Registrador_Deslocamento_Contador_Binario.ino
@@ -1,14 +1,59 @@
+#include<math.h>
 byte trava = 9;
-byte clock = 10;
-byte data = 11; 
+byte relogio = 10;
+byte bits = 11; 
+
+byte numeroBinario[8] = {0,0,0,0,0,0,0,0};
 
 void setup() {
+  Serial.begin(9600);
+  Serial.flush();
+
   pinMode(trava, OUTPUT);
-  pinmode(clock, OUTPUT);
-  pinMode(data , OUTPUT);
+  pinMode(relogio, OUTPUT);
+  pinMode(bits , OUTPUT);
+
+  digitalWrite(trava, LOW);
+  digitalWrite(relogio, LOW);
+  digitalWrite(bits, LOW);
 }
 
 void loop() {
-  
+  for(int i=0; i<255; i++){
+    transformaBinario(i);
+    setLed();
+    Serial.print("Número atual: ");
+    Serial.println(i);
+    delay(1000);
+  }
+}
 
+void transformaBinario(int num){
+  byte *ponteiroBinario = numeroBinario;
+  int y = 1;
+  int numerador = num;
+  for(int x=1; x<9;x++){
+    if(num-pow(2, x)<0){
+      break;
+    }
+    y += 1;
+  }
+  for(int x=y; x>-1;x--){
+    *ponteiroBinario = numerador%2;
+     numerador = numerador/2;
+     ponteiroBinario += 1;
+  }
+}
+
+void setLed(){
+  byte *ponteiroBinario = numeroBinario;
+  digitalWrite(trava, LOW);
+  for(int x=0; x<9; x++){
+    if (*ponteiroBinario==1){digitalWrite(bits, HIGH);}
+    else{digitalWrite(bits, LOW);}
+    ponteiroBinario += 1;
+    digitalWrite(relogio, HIGH);
+    digitalWrite(relogio, LOW);
+  } 
+  digitalWrite(trava, HIGH);
 }
