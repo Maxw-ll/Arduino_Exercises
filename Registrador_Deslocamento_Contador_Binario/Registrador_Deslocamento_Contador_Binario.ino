7ab0df49a79259397c03b7edb91315526883eadf
Maxwell <maxwellpires2002@gmail.com>
2023-01-24 09:39:11 -0300

    Att após aprender alocação dinâmica

--- Registrador_Deslocamento_Contador_Binario/Registrador_Deslocamento_Contador_Binario.ino
+++ Registrador_Deslocamento_Contador_Binario/Registrador_Deslocamento_Contador_Binario.ino
@@ -1,9 +1,10 @@
 #include<math.h>
+#include <stdlib.h>
+
 byte trava = 9;
 byte relogio = 10;
 byte bits = 11; 
 
-byte numeroBinario[8] = {0,0,0,0,0,0,0,0};
 
 void setup() {
   Serial.begin(9600);
@@ -19,18 +20,19 @@ void setup() {
 }
 
 void loop() {
-  for(int i=0; i<255; i++){
-    for(int x=0; x<9; x++){numeroBinario[x]=0;}
-    transformaBinario(i);
-    setLed();
+  byte *numeroBinario = (byte *)malloc(sizeof(byte)*8);
+  for(int i=0; i<256; i++){
+    for(int x=0; x<8; x++){numeroBinario[x]=0;}
+    transformaBinario(i, numeroBinario);
+    setLed(numeroBinario);
     Serial.print("Número atual: ");
     Serial.println(i);
     delay(1000);
   }
+  free(numeroBinario);
 }
 
-void transformaBinario(int num){
-  byte *ponteiroBinario = numeroBinario;
+void transformaBinario(int num, byte *binary_number){
   int y = 1;
   int numerador = num;
   for(int x=1; x<9;x++){
@@ -39,20 +41,17 @@ void transformaBinario(int num){
     }
     y += 1;
   }
-  for(int x=y; x>-1;x--){
-    *ponteiroBinario = numerador%2;
-     numerador = numerador/2;
-     ponteiroBinario += 1;
+  for(int x=7; x>7-y;x--){
+    binary_number[x] = numerador%2;
+    numerador = numerador/2;
   }
 }
 
-void setLed(){
-  byte *ponteiroBinario = numeroBinario;
+void setLed(byte *binary_number){
   digitalWrite(trava, LOW);
-  for(int x=0; x<9; x++){
-    if (*ponteiroBinario==1){digitalWrite(bits, HIGH);}
+  for(int x=7; x>-1; x--){
+    if (binary_number[x]==1){digitalWrite(bits, HIGH);}
     else{digitalWrite(bits, LOW);}
-    ponteiroBinario += 1;
     digitalWrite(relogio, HIGH);
     digitalWrite(relogio, LOW);
   } 
