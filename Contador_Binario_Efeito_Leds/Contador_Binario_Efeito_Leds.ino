4b56627c11ed37cab1c7ff738b28fb3a672f413b
Maxwell <maxwellpires2002@gmail.com>
2023-01-24 10:26:03 -0300

    Att após aprender alocação dinâmica

--- Contador_Binario_Efeito_Leds/Contador_Binario_Efeito_Leds.ino
+++ Contador_Binario_Efeito_Leds/Contador_Binario_Efeito_Leds.ino
@@ -1,11 +1,9 @@
 #include<math.h>
+#include <stdlib.h>
 byte trava = 9;
 byte relogio = 10;
 byte bits = 11;
 
-byte numeroBinario[8] = {0,0,0,0,0,0,0,0};
-
-int x = 1;
 
 void setup() {
   pinMode(trava, OUTPUT);
@@ -18,41 +16,50 @@ void setup() {
 }
 
 void loop() {
-  if(x==256){
-    x=1;
-    for(int x=0; x<9; x++){
+  int x = 1;
+  byte *numeroBinario = (byte*)malloc(sizeof(byte)*8);
+
+  for(int x=0; x<8; x++){
       numeroBinario[x] = 0;
-    }
   }
-  transformaBinario(x);
-  setLed();
-  delay(1000);
-  x = x*2;
-  
+
+  while(x <= 128){
+    transformaBinario(x, numeroBinario);
+    setLed(numeroBinario);
+    delay(100);
+    x *= 2;
+  }
+  x = 128;
+  while(x > 0){
+    transformaBinario(x, numeroBinario);
+    setLed(numeroBinario);
+    delay(100);
+    x /= 2;
+  }
+
+  free(numeroBinario);
 }
 
-void transformaBinario(int numero){
-  byte *ponteiroBinario = numeroBinario;
+void transformaBinario(int numero, byte *binary_number){
   int numerador = numero;
-  int y = 0;
+  int y = 1;
   for(int x=0; x<9; x++){
-    if(numero-(pow(2, x))<0){break;}
+    if(numero-(pow(2, x))<0){
+      break;
+    }
     y += 1;
   }
-  for(int x=y; x>-1; x--){
-    *ponteiroBinario = numerador%2;
+  for(int x=7; x>7-y; x--){
+    binary_number[x] = numerador%2;
     numerador= numerador/2;
-    ponteiroBinario+=1;
   }
 }
 
-void setLed(){
+void setLed(byte *binary_number){
   digitalWrite(trava, LOW);
-  byte *ponteiroBinario = numeroBinario;
-  for(int x=0; x<9; x++){
-    if(*ponteiroBinario==1){digitalWrite(bits, HIGH);}
+  for(int x=7; x>-1; x--){
+    if(binary_number[x]==1){digitalWrite(bits, HIGH);}
     else{digitalWrite(bits, LOW);}
-    ponteiroBinario+=1;
     digitalWrite(relogio, HIGH);
     digitalWrite(relogio, LOW);
   }
